599bb580c18c48b8076323d23c656a48e340a4f0
alaguna95 <albertolh3@gmail.com>
2022-04-06 10:57:20 +0200

    Format to google java format

--- src/main/java/com/alaguna95/architecture/ArchitectureApplication.java
+++ src/main/java/com/alaguna95/architecture/ArchitectureApplication.java
@@ -11,73 +11,62 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 public class ArchitectureApplication {
 
-    public static void main(String[] args) {
-        System.out.println("Hola me llamo alberto estoy probando el formateo que funcione, sabes por que no me " +
-                           "funciona, yo creo que si escribes normal si te funciona, solo tienes que escribir de a " +
-                           "poco u entonces Hola me llamo alberto estoy probando el formateo que funcione, sabes " +
-                           "por que no me funciona, yo creo que si escribes normal si te funciona, solo tienes que " +
-                           "escribir de a oco u en tonce");
+  public static void main(String[] args) {
+    System.out.println(
+        "Hola me llamo alberto estoy probando el formateo que funcione, sabes por que no me funciona, yo creo que "
+            + "si escribes normal si te funciona, solo tienes que escribir de a me llamo akberto laguna me repito "
+            + "addd a aa"
+            + "poco u entonces Hola me llamo alberto estoy probando el formateo que funcione, sabes "
+            + "por que no me funciona, yo creo que si escribes normal si te funciona, solo tienes que "
+            + "escribir de a oco u en tonce");
 
+    List<String> strings = new ArrayList<>();
 
-        List<String>
-                strings =
-                new ArrayList<>();
+    strings.stream()
+        .filter(a -> a.equals("Pruebas de efecto"))
+        .filter(
+            a ->
+                a.equals(
+                    "Prueba de "
+                        + "sdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd "
+                        + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaa"
+                        + "aaaaaaaaaaaaaaaaa"
+                        + "ddddddddddddd"
+                        + "sdddddddddd"))
+        .map(a -> a + "me llamo josesfa")
+        .map(
+            a -> {
+              return a + a;
+            })
+        .collect(Collectors.toList());
 
-        strings.stream()
-                .filter(a -> a.equals("Pruebas de efecto"))
-                .filter(a -> a.equals("Prueba de " +
-                                      "sdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd " +
-                                      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaa" +
-                                      "aaaaaaaaaaaaaaaaa" +
-                                      "ddddddddddddd" +
-                                      "sdddddddddd"))
-                .map(a -> a + "me llamo josesfa")
-                .map(a -> {
-                    return a + a;
-                })
-                .collect(Collectors.toList());
+    SpringApplication.run(ArchitectureApplication.class, args);
+  }
 
-        SpringApplication.run(ArchitectureApplication.class, args);
+  private String test() {
+
+    for (int a = 0; a < 10; a++) System.out.println("prueba");
+
+    if (true) System.out.println("aa");
+
+    if (false) {
+      System.out.println("b");
     }
 
-    private String test() {
+    return 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1
+            && 1 == 1 && 1 == 1
+        ? "pesssssssdrito"
+        : "jusssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss  "
+            + "sssssssssssssssssssssssab";
+  }
 
-        for (int
-             a =
-             0; a < 10; a++) {
-            System.out.println("prueba");
-        }
+  private String
+      testttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttsaasdass(
+          Integer a) throws IOException {
+    throw new IOException("sd");
+  }
 
-
-        if (true) {
-            System.out.println("aa");
-        }
-
-        if (false) {
-            System.out.println("b");
-        }
-
-        return 1 == 1 &&
-               1 == 1 &&
-               1 == 1 &&
-               1 == 1 &&
-               1 == 1 &&
-               1 == 1 &&
-               1 == 1 &&
-               1 == 1 &&
-               1 == 1 &&
-               1 == 1 &&
-               1 == 1 ?
-                "pesssssssdrito" :
-                "jusssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss  " +
-                "sssssssssssssssssssssssab";
-    }
-
-    private String testttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttsaasdass(Integer a) throws IOException {
-        throw new IOException("sd");
-    }
-
-    private String test2() {
-        return 1 == 1 ? "a" : "b";
-    }
+  private String test2() {
+    return 1 == 1 ? "a" : "b";
+  }
 }
--- src/main/java/com/alaguna95/architecture/ThisIsASampleClass.java
+++ src/main/java/com/alaguna95/architecture/ThisIsASampleClass.java
@@ -2,13 +2,13 @@ package com.alaguna95.architecture;
 
 public class ThisIsASampleClass {
 
-    //<title>Title me llamo alberto, como , ocawdsssssssssss,
-    //dasssssssssssssssssssssssssssssssssssssssdddddddddddddddddddddddddddd ssssssss sssssssssssss</title>
-    private static final String
-            PRUEBA_AAAAAAAAAA =
-            "<title>Title me llamo alberto, como , ocawdsssssssssss," +
-            "dasssssssssssssssssssssssssssssssssss dasssssss " +
-            "ssssssssssssssssssssssssssssdasssssssssssssssss " +
-            "ssssssssssssssssssdasssssssssssssssssssssssssssssssssss  " +
-            "ssssdddddddddddddddddddddddddddd ssssssss sssssssssssss</title>";
+  // <title>Title me llamo alberto, como , ocawdsssssssssss,
+  // dasssssssssssssssssssssssssssssssssssssssdddddddddddddddddddddddddddd ssssssss
+  // sssssssssssss</title>
+  private static final String PRUEBA_AAAAAAAAAA =
+      "<title>Title me llamo alberto, como , ocawdsssssssssss,"
+          + "dasssssssssssssssssssssssssssssssssss dasssssss "
+          + "ssssssssssssssssssssssssssssdasssssssssssssssss "
+          + "ssssssssssssssssssdasssssssssssssssssssssssssssssssssss  "
+          + "ssssdddddddddddddddddddddddddddd ssssssss sssssssssssss</title>";
 }
