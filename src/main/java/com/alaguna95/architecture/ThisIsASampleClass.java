54c98da3db40196f35922866d1cf4f23e284b45b
Alberto Laguna Humayor <alaguna@hiberus.com>
2022-04-06 09:58:32 +0200

    first commit

---
+++ src/main/java/com/alaguna95/architecture/ThisIsASampleClass.java
@@ -0,0 +1,14 @@
+package com.alaguna95.architecture;
+
+public class ThisIsASampleClass {
+
+    //<title>Title me llamo alberto, como , ocawdsssssssssss,
+    //dasssssssssssssssssssssssssssssssssssssssdddddddddddddddddddddddddddd ssssssss sssssssssssss</title>
+    private static final String
+            PRUEBA_AAAAAAAAAA =
+            "<title>Title me llamo alberto, como , ocawdsssssssssss," +
+            "dasssssssssssssssssssssssssssssssssss dasssssss " +
+            "ssssssssssssssssssssssssssssdasssssssssssssssss " +
+            "ssssssssssssssssssdasssssssssssssssssssssssssssssssssss  " +
+            "ssssdddddddddddddddddddddddddddd ssssssss sssssssssssss</title>";
+}
---
+++ src/test/java/com/alaguna95/architecture/ArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaguna95.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
