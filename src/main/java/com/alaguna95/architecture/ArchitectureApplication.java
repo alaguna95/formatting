149f735a486d3a6de02ad461b4207634e279bb91
alberto.laguna <V542832@alliance-healthcare.com>
2022-12-29 09:25:10 +0100

    Change the way of format. Also add google-java-formatter code-style

--- src/main/java/com/alaguna95/architecture/ArchitectureApplication.java
+++ src/main/java/com/alaguna95/architecture/ArchitectureApplication.java
@@ -11,61 +11,76 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArchitectureApplication {
 
   public static void main(String[] args) {
+
     System.out.println(
-        "Hola me llamo alberto estoy probando el formateo que funcione, sabes por que no me"
-            + " funciona, yo creo que si escribes normal si te funciona, solo tienes que escribir"
-            + " de a me llamo akberto laguna me repito addd a aapoco u entonces Hola me llamo"
-            + " alberto estoy probando el formateo que funcione, sabes por que no me funciona, yo"
+        "Hola me llamo alberto estoy probando el formateo que funcione, sabes por que no me funciona, "
+            + "yo creo que si escribes normal si te funciona, solo tienes que escribir de a me llamo akberto laguna"
+            + " me repito addd a aapoco u entonces Hola me llamo aaaaaaaaaaaa "
+            + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaa ssssssssssssss sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+            + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            + " alberto           estoy probando el formateo que funcione, sabes por que no me funciona, yo"
             + " creo que si escribes normal si te funciona, solo tienes que escribir de a oco u en"
             + " tonce");
 
     List<String> strings = new ArrayList<>();
 
-    strings.stream()
-        .filter(a -> a.equals("Pruebas de efecto"))
-        .filter(
-            a ->
-                a.equals(
-                    "Prueba de "
-                        + "sdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd "
-                        + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaa"
-                        + "aaaaaaaaaaaaaaaaa"
-                        + "ddddddddddddd"
-                        + "sdddddddddd"))
-        .map(a -> a + "me llamo josesfa")
-        .map(
-            a -> {
-              return a + a;
-            })
-        .collect(Collectors.toList());
+    strings.stream().filter(a -> a.equals("Pruebas de efecto")).filter(a -> a.equals(
+            "Prueba de sdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd "
+                + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaa"
+                + "aaaaaaaaaaaaaaaaa"
+                + "ddddddddddddd"
+                + "sdddddddddd"))
+        .map(a -> a + "me llamo josesfa").map(a -> {
+          return a + a;
+        }).collect(Collectors.toList());
 
     SpringApplication.run(ArchitectureApplication.class, args);
   }
 
   private String test() {
 
-    for (int a = 0; a < 10; a++) System.out.println("prueba");
+    int x = (3 + 4 + 5 + 6) * (7 + 8 + 9 + 10) * (11 + 12 + 13 + 14 + 0xFFFFFFFF);
 
-    if (true) System.out.println("aa");
+    for (int a = 0; a < 10; a++) {
+      System.out.println("prueba");
+    }
 
+    if (true) {
+      System.out.println("aa");
+    }
     if (false) {
       System.out.println("b");
     }
 
     return 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1 && 1 == 1
-            && 1 == 1 && 1 == 1
+        && 1 == 1 && 1 == 1
         ? "pesssssssdrito"
-        : "jusssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss  "
+        : "jusssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss                             "
+            + "ssssssssssss  "
             + "sssssssssssssssssssssssab";
   }
 
-  private String
-      testttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttsaasdass(
-          Integer a) throws IOException {
+  private String tttttttttttttttttttttttttttttttsaasdass(Integer a) throws IOException {
     throw new IOException("sd");
   }
 
   private String test2() {
     return 1 == 1 ? "a" : "b";
+  }
+
+  private boolean method1() {
+    return true;
+  }
+
+  private boolean method2() {
+    return true;
+  }
+
+  private boolean method3() {
+    return true;
+  }
+
+  private boolean method4() {
+    return true;
   }
 }
